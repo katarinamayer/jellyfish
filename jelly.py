f6ce75db67ec822786f1dfc8d1ef2662cb5a1b8a
katarinamayer <katarina.mayer@jhu.edu>
2020-03-22 14:43:23 -0400

    more skeleton and initializtation

--- jelly.py
+++ jelly.py
@@ -1,5 +1,9 @@
 from mininet.net import Mininet
 from mininet.topo import Topo
+from mininet.util import dumpNodeConnections
+
+import os
+import sys
 # need to import a bunch of mininet shit
 
 
@@ -41,16 +45,49 @@ topos = { 'mytopo': ( lambda: MyTopo() ) }
 class constructGraph():
 
 	#initialize nums
-	def __init__(self, nodes, ports, switches):
+	def __init__(self, numNodes, numPorts, numSwitches):
+        self.numNodes = numNodes
+        self.numPorts = numPorts
+        self.numSwitches = numSwitches
 
-
+    '''
+    see https://github.com/mininet/mininet/wiki/Introduction-to-Mininet
+    class SingleSwitchTopo(Topo):
+        "Single switch connected to n hosts."
+        def build(self, n=2):
+            switch = self.addSwitch('s1')
+            # Python's range(N) generates 0..N-1
+            for h in range(n):
+                host = self.addHost('h%s' % (h + 1))
+                self.addLink(host, switch)
+    '''
 
 	#algo to create graph
-	def createGraph(self):
+	def build(self):
+
+        hosts = []
+        for i in range(numNodes):
+            hosts.append(self.addHost('h' + str(i)))
+
+        switches = []
+        for i in range(numSwitches):
+            switches.append(self.addHost('s' + str(i))
+
+        #randomly pick a pair of (non-neighboring) switches with free ports.
 
 
+
+# initalize the actual mininet
 def main():
 
+    #get cmd line args for params in future
+    numNodes = 10
+    numPorts = 10
+    numSwitches = 10
 
+    topo = constructGraph(numNodes, numPorts, numSwitches)
+    network = Mininet(topo)
 
-
+    network.start()
+    network.pingAll()
+    network.stop()
