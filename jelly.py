ea995e1d015a2542eaf9769d05e90dc3e561b1c9
katarinamayer <katarina.mayer@jhu.edu>
2020-03-23 16:15:10 -0400

    aljsjngdag

--- jelly.py
+++ jelly.py
@@ -22,24 +22,24 @@ topology enables one to pass in '--topo=mytopo' from the command line.
 from mininet.topo import Topo
 
 class MyTopo( Topo ):
-	"Simple topology example."
+    "Simple topology example."
 
-	def __init__( self ):
-		"Create custom topo."
+    def __init__( self ):
+        "Create custom topo."
 
-		# Initialize topology
-		Topo.__init__( self )
+        # Initialize topology
+        Topo.__init__( self )
 
-		# Add hosts and switches
-		leftHost = self.addHost( 'h1' )
-		rightHost = self.addHost( 'h2' )
-		leftSwitch = self.addSwitch( 's3' )
-		rightSwitch = self.addSwitch( 's4' )
+        # Add hosts and switches
+        leftHost = self.addHost( 'h1' )
+        rightHost = self.addHost( 'h2' )
+        leftSwitch = self.addSwitch( 's3' )
+        rightSwitch = self.addSwitch( 's4' )
 
-		# Add links
-		self.addLink( leftHost, leftSwitch )
-		self.addLink( leftSwitch, rightSwitch )
-		self.addLink( rightSwitch, rightHost )
+        # Add links
+        self.addLink( leftHost, leftSwitch )
+        self.addLink( leftSwitch, rightSwitch )
+        self.addLink( rightSwitch, rightHost )
 
 
 topos = { 'mytopo': ( lambda: MyTopo() ) }
@@ -47,119 +47,117 @@ topos = { 'mytopo': ( lambda: MyTopo() ) }
 
 class Jellyfish(Topo):
 
-	#initialize nums
-	def __init__(self, numNodes, numPorts, numServerPorts, numSwitches):
-		super(Jellyfish, self).__init__()
-		self.numNodes = numNodes
-		self.numPorts = numPorts
-		self.numServerPorts = numServerPorts
-		self.numSwitches = numSwitches
-		self.build()
+    #initialize nums
+    def __init__(self, numNodes, numPorts, numServerPorts, numSwitches):
+        super(Jellyfish, self).__init__()
+        self.numNodes = numNodes
+        self.numPorts = numPorts
+        self.numServerPorts = numServerPorts
+        self.numSwitches = numSwitches
+        self.build()
 
-		# Initialize graph of switch topology using networkx
+        # Initialize graph of switch topology using networkx
  #       self.graph = nx.Graph()
  #       self.graph.add_nodes_from(['s'+str(i) for i in range(numSwitches)])
 
-	#algo to create graph
-	def build(self):
-        print("x")
-		hosts = []
-        print('here')
-		for i in range(self.numNodes):
-			hosts.append(self.addHost('h' + str(i)))
-			print(hosts[i])
+    #algo to create graph
+    def build(self):
+        hosts = []
+        for i in range(self.numNodes):
+            hosts.append(self.addHost('h' + str(i)))
+            print(hosts[i])
 
-		switches = []
-		ports = []
-		for i in range(self.numSwitches):
-			switches.append(self.addSwitch('s' + str(i)))
-			ports.append(self.numPorts)
-			# each switch has all open ports at this point
+        switches = []
+        ports = []
+        for i in range(self.numSwitches):
+            switches.append(self.addSwitch('s' + str(i)))
+            ports.append(self.numPorts)
+            # each switch has all open ports at this point
 
-		#Connect each host to a switch --> ASSUME EQUAL NUM OF EACH (for now)
-		for i in range(self.numNodes):
-			self.addLink(hosts[i], switches[i])
-			ports[i] -= 1
+        #Connect each host to a switch --> ASSUME EQUAL NUM OF EACH (for now)
+        for i in range(self.numNodes):
+            self.addLink(hosts[i], switches[i])
+            ports[i] -= 1
 
-		'''
-		Randomly pick a pair of (non-neighboring)
-		switches with free ports, join them with a link,
-		repeat until no further links can be added.
-		'''
+        '''
+        Randomly pick a pair of (non-neighboring)
+        switches with free ports, join them with a link,
+        repeat until no further links can be added.
+        '''
 
-		# Track adjacent switches
-		adjacent = set()
-		#Track switch 1
-		marked_switch1 = []
+        # Track adjacent switches
+        adjacent = set()
+        #Track switch 1
+        marked_switch1 = []
 
-		# Loop through all switches
-		while len(marked_switch1) < self.numSwitches:
-			index1 = randrange(self.numSwitches)
+        # Loop through all switches
+        while len(marked_switch1) < self.numSwitches:
+            index1 = randrange(self.numSwitches)
 
-			# Check that switch has not been marked and that it has open ports
-			while index1 not in marked_switch1 and ports[index1] > 0:
+            # Check that switch has not been marked and that it has open ports
+            while index1 not in marked_switch1 and ports[index1] > 0:
 
-				# Track switch2
-				marked_switch2 = []
-				index2 = randrange(self.numSwitches)
+                # Track switch2
+                marked_switch2 = []
+                index2 = randrange(self.numSwitches)
 
-				# Check that switch has not been marked, is not equal to index1 and has open ports
-				while index2 not in marked_switch2 and index2 != index1 and ports[index2] > 0:
+                # Check that switch has not been marked, is not equal to index1 and has open ports
+                while index2 not in marked_switch2 and index2 != index1 and ports[index2] > 0:
 
-					# Check that the links are not adjacent
-					while (index1, index2) not in adjacent:
+                    # Check that the links are not adjacent
+                    while (index1, index2) not in adjacent:
 
-						# Form new link
-						self.addLink(switches[index1], switches[index2])
-						ports[index1] -= 1
-						ports[index2] -= 1
+                        # Form new link
+                        self.addLink(switches[index1], switches[index2])
+                        ports[index1] -= 1
+                        ports[index2] -= 1
 
-						# Add new link to set to track adjacency
-						adjacent.add((index1, index2))
-						adjacent.add((index2, index1))
+                        # Add new link to set to track adjacency
+                        adjacent.add((index1, index2))
+                        adjacent.add((index2, index1))
 
-						# Mark swtich2
-						marked_switch2.append(index2)
+                        # Mark swtich2
+                        marked_switch2.append(index2)
 
-			# Mark switch 1
-			marked_switch1.append(index1)
+            # Mark switch 1
+            marked_switch1.append(index1)
 
 
-		'''
-		For sampling non-neighbors, just shuffle a list of non-neighbors, iterate through,
-		and check whether each node is in a set of closed ports
-		'''
+        '''
+        For sampling non-neighbors, just shuffle a list of non-neighbors, iterate through,
+        and check whether each node is in a set of closed ports
+        '''
 
 # #TODO
 # def get_args():
-# 	parser = argparse.ArgumentParser()
-# 	parser.add_argument('--numNodes', help="Number of hosts", required=True)
-# 	parser.add_argument('--numPorts', help="Number of total ports per switch", required=True)
-# 	parser.add_argument('--numServerPorts', help="Number of ports per switch to reserve to servers", required=True)
-# 	parser.add_argument('--numSwitches', help="Number of Switches", required=True)
-# 	return parser.parse_args()
+#   parser = argparse.ArgumentParser()
+#   parser.add_argument('--numNodes', help="Number of hosts", required=True)
+#   parser.add_argument('--numPorts', help="Number of total ports per switch", required=True)
+#   parser.add_argument('--numServerPorts', help="Number of ports per switch to reserve to servers", required=True)
+#   parser.add_argument('--numSwitches', help="Number of Switches", required=True)
+#   return parser.parse_args()
 
 
 # initalize the actual mininet
 def main():
-	'''
-	args = get_args()
-	numNodes = args.numNodes
-	numPorts = args.numPorts
-	numServerPorts = arsg.numServerPorts
-	numSwitches = args.numSwitches
-	'''
-	numNodes = 10
-	numPorts = 10
-	numServerPorts = 5
-	numSwitches = 10
+    '''
+    args = get_args()
+    numNodes = args.numNodes
+    numPorts = args.numPorts
+    numServerPorts = arsg.numServerPorts
+    numSwitches = args.numSwitches
+    '''
+    numNodes = 10
+    numPorts = 10
+    numServerPorts = 5
+    numSwitches = 10
 
-	topo = Jellyfish(numNodes=numNodes, numPorts=numPorts, numServerPorts=numServerPorts, numSwitches=numSwitches)
-	network = Mininet(topo=topo)
+    topo = Jellyfish(numNodes=numNodes, numPorts=numPorts, numServerPorts=numServerPorts, numSwitches=numSwitches)
+    network = Mininet(topo=topo)
 
-	network.start()
-	network.pingAll()
-	network.stop()
+    network.start()
+    network.pingAll()
+    network.stop()
 
 if __name__ == '__main__':
-	main()
+    main()
