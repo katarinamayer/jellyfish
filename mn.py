2562d851aafbaff4f3949d35157a6ef1b9e9baca
katarinamayer <kat@cs419-project.us-east4-c.c.resolute-parity-271020.internal>
2020-04-07 06:12:32 +0000

    transferring files

---
+++ mn.py
@@ -0,0 +1,17 @@
+"""Custom topologies for Mininet
+
+author: Brandon Heller (brandonh@stanford.edu)
+
+To use this file to run a RipL-specific topology on Mininet.  Example:
+
+  sudo mn --custom ~/ripl/ripl/mn.py --topo ft,4
+"""
+
+from ripl.dctopo import FatTreeTopo #, JellyfishTopo #, VL2Topo, TreeTopo
+from ripl.jelly import Jellyfish 
+
+
+topos = { 'ft': FatTreeTopo, 'jelly': Jellyfish}
+#,
+#          'vl2': VL2Topo,
+#          'tree': TreeTopo }
