907aa7a6e6a9b35a8d27e81c62e458e09a7fb437
katarinamayer <katarina.mayer@jhu.edu>
2020-04-09 15:56:04 -0400

    writing prescript to save graph files

--- jelly_prescript.py
+++ jelly_prescript.py
@@ -6,29 +6,64 @@ import networkx
 import random
 import matplotlib.pyplot as plt
 import pickle
+from graphviz import Graph
+import networkx as nx
+
+from graph import Jellyfish
 
 def compute_ecmp(graph): # graph will be a network_x graph
-	return 0
+    return 0
 
 def compute_ksp(graph):
-	return 0
+    return 0
 
 
 
-''' TODO Kat '''
-def build_graph(nNodes, nPorts): 
-	''' 
-	nNodes = number of switches
-	transfer graph building logic here
-	convert to network_x graph
-	output adjacency list to store graph
-	'''
+''' Get graph, convert to networkx graph, save ADJLIST file to store graph '''
+def get_graph(nSwitches, nPorts): 
 
-	return 0
+    j = Jellyfish(nSwitches, nPorts)
+    edge_list = j.adjacent
+
+    G = nx.Graph()
+    added = []
+    for edge in edge_list:
+        node1 = edge[0]
+        node2 = edge[1]
+        if((node2, node1) not in added):
+            G.add_edge(node1, node2)
+            added.append(edge)
+
+    nx.write_adjlist(G, "test.adjlist")
+
+
+    nx.draw(G)
+    plt.show()
+
+    return G
+
+
+''' Simple function to visualize graph for testing '''
+def visualize_graph(edge_list):
+    dot = Graph()
+    added = []
+    for link in edge_list:
+        node1 = link[0]
+        node2 = link[1]
+
+        if((node2, node1) not in added):
+            dot.edge(str(node1), str(node2))
+            added.append(link)
+    dot.view()
+
 
 
 def main():
-	''' create files '''
+    ''' create files '''
+
+    # testing
+    graph = get_graph(80, 5)
+    #visualize_graph(graph)
 
 
 if __name__ == '__main__':
