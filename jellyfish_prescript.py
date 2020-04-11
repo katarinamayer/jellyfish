4b9e3fbd043c93aeb49662709e36ed3770d2215d
lurnt <laurentpark98@gmail.com>
2020-04-11 15:10:53 -0400

    moved open source functions to jelly_utils.py

--- jellyfish_prescript.py
+++ jellyfish_prescript.py
@@ -18,35 +18,6 @@ from jellyfish_graph import Jellyfish
 TRANSFORM_DIR = 'transformed_routes'
 PKL_DIR = 'pickled_routes'
 
-def save_obj(obj, path):
-    with open(path, 'wb') as f:
-        pickle.dump(obj,f)
-
-def load_obj(path):
-    with open(path, 'rb') as f:
-        return pickle.load(f)
-
-def compute_ecmp(graph): 
-    '''
-    Assumptions made: nodes are labelled from 0 to n,
-    where n is the number of nodes in the graph
-    '''
-    n = graph.number_of_nodes()
-    ecmp_paths = {}
-    for src in range(n):
-        for dst in range(src+1, n):
-            shortest_paths = nx.all_shortest_paths(graph, source=src, target=dst)
-            ecmp_paths[(str(src), str(dst))] = [p for p in shortest_paths]
-    return ecmp_paths
-
-def compute_ksp(graph, k=8):
-    n = graph.number_of_nodes()
-    all_ksp = {}
-    for a in range(n):
-        for b in range(src+1, n):
-            ksp = list(islice(nx.shortest_simple_paths(networkx_graph, source=str(src), target=str(dst)), k))
-            all_ksp[(str(src), str(dst))] = ksp
-    return all_ksp
 
 
 ''' Get graph, convert to networkx graph, save ADJLIST file to store graph '''
@@ -78,14 +49,14 @@ def main():
 
     ''' output routing files '''
     filename = 'test_1'
-    ecmp_routes = compute_ecmp(graph)
+    ecmp_routes = util.compute_ecmp(graph)
     ecmp_path = os.path.join(PKL_DIR, 'ecmp_{}.pkl'.format(filename))
-    save_obj(ecmp_routes, ecmp_path)
+    util.save_obj(ecmp_routes, ecmp_path)
 
     k = 8
-    t_ecmp_routes = util.transform_paths_dpid('ecmp_{}'.format(filename), k)
+    t_ecmp_routes = util.transform_paths_dpid(ecmp_path, k)
     t_ecmp_path = os.path.join(TRANSFORM_DIR, 'ecmp_{}_{}.pkl'.format(k, filename))
-    save_obj(t_ecmp_routes, t_ecmp_path)
+    util.save_obj(t_ecmp_routes, t_ecmp_path)
 
 if __name__ == '__main__':
     main()
