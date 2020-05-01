d30502f680909dba19ce5b0664244b9fd7bf617b
katarinamayer <katarina.mayer@jhu.edu>
2020-04-30 23:42:48 -0400

    added additional r/w permissions to test.sh

--- jellyfish/test.sh
+++ jellyfish/test.sh
@@ -7,15 +7,15 @@ do
 	if [ -d "$DIR$x" ]; then
 		continue
 	else
-		mkdir "$DIR$x"
+		sudo mkdir "$DIR$x"
 		echo "created new directory $DIR$x"
 
-		cp perftest/results/*.txt "$DIR$x"
-		sudo rm perftest/results/*.txt
+		sudo mv perftest/results/*.txt "$DIR$x"
+		# sudo rm perftest/results/*.txt
 		echo "moved test files into $DIR$x"
 
 		break
-
 	fi
+	
 done
 
